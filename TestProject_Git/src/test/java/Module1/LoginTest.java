37c71737778a3372aa2752317139b6864003898f
KV00523493 <KV00523493@INPUCZPC00088.TechMahindra.com>
2019-01-31 15:07:57 +0530

    3rd commit by me

--- TestProject_Git/src/test/java/Module1/LoginTest.java
+++ TestProject_Git/src/test/java/Module1/LoginTest.java
@@ -2,4 +2,7 @@ package Module1;
 
 public class LoginTest {
 
+	private void Syso() {		// TODO Auto-generated method stub
+           
+	}
 }
---
+++ TestProject_Git/src/test/java/Module1/PaymentTest.java
@@ -0,0 +1,11 @@
+package Module1;
+
+public class PaymentTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+             System.out.println("OKay");		
+	}
+
+}
